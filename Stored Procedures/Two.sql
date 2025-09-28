246d0bf7626ebc4a7754c31434d4c2df52ece8a3
Shrey K <srkkadia@gmail.com>
2025-09-28 12:01:25 +0530

    stored procedures

--- Stored Procedures/Two.sql
+++ Stored Procedures/Two.sql
@@ -33,3 +33,68 @@ SELECT @var_name;
 -------------------------------------------------------------
 
 -- Alteration of a procedure
+DELIMITER //
+
+CREATE PROCEDURE GetEmployees()
+BEGIN
+    SELECT * FROM employees;
+END //
+
+DELIMITER ;
+
+-------------------------------------------------------------
+SHOW CREATE PROCEDURE GetEmployees\G;
+
+-------------------------------------------------------------
+ALTER PROCEDURE GetEmployees COMMENT "Get Employees";
+
+-------------------------------------------------------------
+-- MySQL Stored Procedure Variables
+
+-- dECLARING vARIABLES
+-- DECLARE variable_name datatype(size) [DEFAULT default_value];
+
+DECLARE totalSale DEC(10, 2) DEFAULT 0.0;
+DECLARE totalQty, stockCount INT DEFAULT 0;
+
+DECLARE amount DECMIAL(10,2);
+DECLARE currency CHAR(3) DEFAULT 'USD';
+
+-------------------------------------------------------------
+-- Assigning Variables
+-- SET variable_name = value;
+
+DECLARE total INT DEFAULT 0;
+SET total = 10;
+
+-------------------------------------------------------------
+
+DELIMITER $$
+
+CREATE PROCEDURE GetTotalOrder()
+BEGIN
+    DECLARE totalOrder INT DEFAULT 0;
+
+    SELECT COUNT(*)
+    INTO totalOrder
+    FROM orders;
+
+    SELECT totalOrder;
+
+END $$
+
+DELIMITER ;
+
+-------------------------------------------------------------
+-- Listing procedures
+
+SHOW PROCEDURE STATUS WHERE db = 'classicmodels';
+-------------------------------------------------------------
+
+SELECT 
+    routine_name
+FROM
+    information_schema.routines
+WHERE
+    routine_type = 'PROCEDURE'
+        AND routine_schema = 'classicmodels';
